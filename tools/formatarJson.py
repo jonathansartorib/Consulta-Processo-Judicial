9fd2137ffb39b27bc4a7ea94d03f367d4f1b799b
Jonathan Sartori Bruzarrosco <jonathansartorib@hotmail.com>
2023-11-30 12:10:28 -0300

    debug

--- tools/formatarJson.py
+++ tools/formatarJson.py
@@ -12,19 +12,20 @@ dados = [
 ]
 
 # Formatando os dados conforme solicitado
-dados_formatados = [
-    {
-        str(item["codigo"]): {
-            "name": item["nome"],
-            "site": "",
-            "tel": ""
-        }
+dados_formatados = {
+    str(item["codigo"]): {
+        "name": item["nome"],
+        "site": "",
+        "tel": ""
     }
     for item in dados
-]
+}
 
-# Convertendo para JSON
-dados_json = json.dumps(dados_formatados, ensure_ascii=False, indent=2)
+# Convertendo para JSON com aspas duplas
+dados_json = json.dumps([dados_formatados], ensure_ascii=False, indent=2, separators=(',', ': '))
+
+# Substituindo as aspas simples por aspas duplas
+dados_json = dados_json.replace("'", "\"")
 
 # Escrevendo no arquivo
 with open('dados_formatados.json', 'w', encoding='utf-8') as file:
